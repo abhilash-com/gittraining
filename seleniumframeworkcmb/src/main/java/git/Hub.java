b7249e037d9a54b30b1d075945eeb5c96cf7e7c4
arachakonda <arachakonda@ARACHAKONDA-DT.ctepl.local>
2022-05-23 15:16:38 +0530

    second commit

---
+++ seleniumframeworkcmb/src/main/java/git/Hub.java
@@ -0,0 +1,11 @@
+package git;
+
+public class Hub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+             
+		System.out.println("my name is vijayalakshmi");
+	}
+
+}
